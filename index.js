d4b6ddeaec8e82ed8b8471e05b68c488e07297ab
Dennis Muuo <dennismuuo.dev@gmail.com>
2025-01-11 14:51:00 +0300

    Added port

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const { Api } = require('telegram');
 const input = require('input');
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Replace these with your actual API credentials from Telegram
 const api_id = 28205301;
@@ -243,3 +246,11 @@ async function fetchTransactionStatus(reference, chatId, client, userId, amount,
     }
   }
 }
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
